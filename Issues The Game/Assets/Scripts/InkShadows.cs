42ce2dc9fd7ba9e66fd910854c1753578176cb11
scara2016 <69334852+scara2016@users.noreply.github.com>
2022-04-28 16:23:07 +1000

    Im done with life

--- Issues The Game/Assets/Scripts/InkShadows.cs
+++ Issues The Game/Assets/Scripts/InkShadows.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class InkShadows : MonoBehaviour
 {
     // Start is called before the first frame update
-    float dropTime;
+    private float dropTime = 0f;
+    public float timeTillDrop = 1f;
     public InkDrops inkDrop;
+    public Vector3 finalScale = new Vector3(3f,3f,3f);
+    private Vector3 initialScale;
+
     void Start()
     {
-        
+        initialScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dropTime += Time.deltaTime;
+        if(dropTime >= timeTillDrop)
+        {
+            GenInkDrops();
+            Destroy(this.gameObject);
+        }
+
+        this.transform.localScale = Vector3.Lerp(initialScale, finalScale, dropTime / timeTillDrop);
     }
+
+    private void GenInkDrops()
+    {
+        Vector2 spawnPosition = this.transform.position;
+        InkDrops clone = Instantiate(inkDrop, spawnPosition, Quaternion.identity);
+        clone.transform.localScale = finalScale;
+    }
+
 }
