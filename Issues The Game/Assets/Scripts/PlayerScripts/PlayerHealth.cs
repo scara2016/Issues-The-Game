0252ae713e12b8be9837eed4dcf0f1c10a829916
Madison Berg <madison.berg@students.mq.edu.au>
2022-05-23 12:38:27 +1000

    added fall death

--- Issues The Game/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Issues The Game/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -156,5 +156,13 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Fall"))
+        {
+            Die();
+        }
+    }
+
 
 }
