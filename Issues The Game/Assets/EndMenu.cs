92de8801790dcc2a54687e618d1bf7c98855be94
scara2016 <guriqbalsingh.majhail@students.mq.edu.au>
2022-06-05 19:58:41 +1000

    Fixed

---
+++ Issues The Game/Assets/EndMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Level 1 (Tutorial)");
+        
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1f;
+    }
+
+
+
+
+}
